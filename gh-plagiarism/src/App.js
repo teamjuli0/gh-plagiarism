7d5bf715cd5dfc9b7dcab14f396e8bd4c6fed169
Julio Valdez <valdezjulio95@gmail.com>
2021-09-13 01:03:50 -0500

    changed some elements and added autofocus on load to input

--- gh-plagiarism/src/App.js
+++ gh-plagiarism/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg'
 import { useRef, useState, useEffect } from 'react'
 import './App.css'
+import './fonts/Noto_Sans/index.css'
+import './fonts/Pacifico/index.css'
 
 const specialChars = '.,:;\'\\`"=*!?#$&+^|~<>(){}[]@ '.split('')
 
@@ -15,61 +16,261 @@ function App() {
 
   const inputEl = useRef()
 
+  function clearStorage() {
+    localStorage.setItem('ghPlagiarismHistory', '[]')
+    setSearchHistory([])
+  }
+
+  function handleKeyUp(e, str) {
+    if (e.keyCode === 13) {
+      checkStr(str)
+    }
+  }
+
   function checkStr(str) {
     let newStr = 'https://github.com/search?q='
 
     const splitStr = str.current.value.trim().split('')
 
-    splitStr.map((char, i) => {
-      if (specialChars.indexOf(char) !== -1) {
-        if (newStr[newStr.length - 1] === '+') {
-          return null
+    if (splitStr.length < 1) {
+      console.log('validation')
+    } else {
+      splitStr.map((char, i) => {
+        if (specialChars.indexOf(char) !== -1) {
+          if (newStr[newStr.length - 1] === '+') {
+            return null
+          } else {
+            return (newStr += '+')
+          }
         } else {
-          return (newStr += '+')
+          return (newStr += char)
         }
-      } else {
-        return (newStr += char)
-      }
-    })
+      })
 
-    if (newStr[newStr.length - 1] === '+') newStr = newStr.slice(0, -1)
+      if (newStr[newStr.length - 1] === '+') newStr = newStr.slice(0, -1)
 
-    window.open((newStr += '&type=code'), '_blank')
+      window.open((newStr += '&type=code'), '_blank')
 
-    setSearchHistory([...searchHistory, newStr])
-    console.log(searchHistory)
+      setSearchHistory([
+        { url: newStr, date: new Date().toLocaleString() },
+        ...searchHistory.slice(0, 99),
+      ])
+      console.log(searchHistory)
+    }
   }
 
   return (
-    <div
+    <main
       style={{
-        height: '600px',
+        minHeight: '561px',
         width: '300px',
-        backgroundColor: 'gray',
+        backgroundColor: '#212121',
+        fontFamily: 'NotoSans',
+        color: '#e8e8e8',
       }}
     >
-      <div
+      <header
         style={{
-          padding: '10px',
+          width: '100%',
           display: 'flex',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
+          backgroundColor: '#424242',
+          marginBottom: '10px',
         }}
       >
-        <input ref={inputEl} />
-        <button onClick={() => checkStr(inputEl)}>Click Me</button>
-      </div>
-      <div>
-        {searchHistory.map((search, i) => (
-          <>
-            <a href={search} target='_blank' rel='noreferrer'>
-              {search.slice(28).slice(0, -10).slice(0, 35)}
-            </a>
-            <br />
-          </>
+        <div
+          style={{
+            display: 'flex',
+            flex: '1 1 10%',
+            padding: '10px 2px',
+            cursor: 'pointer',
+          }}
+        >
+          <button
+            style={{
+              background: 'none',
+              color: 'inherit',
+              font: 'inherit',
+              cursor: 'pointer',
+              outline: 'inherit',
+              flex: '1 1 100%',
+              border: 0,
+              padding: '3px 0',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onClick={() => clearStorage()}
+          >
+            <i style={{ color: '#cccccc' }} class='fas fa-ban'></i>
+          </button>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flex: '1 1 80%',
+            padding: '10px 0',
+          }}
+        >
+          <input
+            style={{
+              flex: '1 1 75%',
+              border: 0,
+              height: '30px',
+              borderRadius: '3px',
+              overflow: 'hidden',
+              padding: '0px 10px',
+              backgroundColor: '#b2b2b2',
+            }}
+            autoFocus
+            placeholder='Enter Your Query Here'
+            onKeyUp={(e) => handleKeyUp(e, inputEl)}
+            ref={inputEl}
+          />
+        </div>
+
+        <div
+          style={{
+            display: 'flex',
+            flex: '1 1 10%',
+            padding: '10px 2px',
+            cursor: 'pointer',
+          }}
+        >
+          <button
+            style={{
+              background: 'none',
+              color: 'inherit',
+              font: 'inherit',
+              cursor: 'pointer',
+              outline: 'inherit',
+              flex: '1 1 100%',
+              border: 0,
+              padding: '3px 0',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onClick={() => checkStr(inputEl)}
+          >
+            <i style={{ color: '#cccccc' }} class='fas fa-search'></i>
+          </button>
+        </div>
+      </header>
+      <section
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+      >
+        {searchHistory.length === 0 ? (
+          <h1
+            style={{
+              width: '100%',
+              padding: '10px 41px',
+              textAlign: 'center',
+              color: '#999999',
+            }}
+          >
+            Enter Search Query Above!
+          </h1>
+        ) : null}
+        {searchHistory.slice(0, 10).map((search, i) => (
+          <div
+            style={
+              i % 2 === 0
+                ? {
+                    backgroundColor: '#424242',
+                    minHeight: '40px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: 'e8e8e8',
+                    flex: '1 1 100%',
+                    flexWrap: 'wrap',
+                    padding: '5px',
+                  }
+                : {
+                    backgroundColor: '#303030',
+                    minHeight: '40px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: 'e8e8e8',
+                    flex: '1 1 100%',
+                    flexWrap: 'wrap',
+                    padding: '5px',
+                  }
+            }
+          >
+            <button
+              style={{
+                background: 'none',
+                color: 'inherit',
+                border: 0,
+                padding: 0,
+                font: 'inherit',
+                cursor: 'pointer',
+                outline: 'inherit',
+                width: '80%',
+                textDecoration: 'none',
+                textAlign: 'left',
+              }}
+              href={search.url}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <p
+                style={{
+                  flex: '1 1 100%',
+                  margin: 0,
+                }}
+              >
+                {search.url.slice(28).slice(0, -10).slice(0, 25)}
+                {search.url.slice(28).slice(0, -10).slice(0, 25).length >= 25
+                  ? '...'
+                  : ''}
+              </p>
+              <p
+                style={{
+                  flex: '1 1 100%',
+                  fontSize: '12px',
+                  color: '#7f7f7f',
+                  margin: 0,
+                }}
+              >
+                Searched on {search.date.split(',')[0]} at{' '}
+                {search.date.split(',')[1]}
+              </p>
+            </button>
+            <button
+              style={{
+                background: 'none',
+                color: 'inherit',
+                border: 0,
+                padding: 0,
+                font: 'inherit',
+                cursor: 'pointer',
+                outline: 'inherit',
+                width: '10%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              onClick={() =>
+                setSearchHistory(
+                  searchHistory.filter((stateSearch) =>
+                    search.date !== stateSearch.date ? true : false
+                  )
+                )
+              }
+            >
+              <i class='fas fa-backspace'></i>
+            </button>
+          </div>
         ))}
-      </div>
-    </div>
+      </section>
+    </main>
   )
 }
 
