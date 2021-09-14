5cefba179d6dfe5a6b9c39b3e9f11fb6402e8648
Julio Valdez <valdezjulio95@gmail.com>
2021-09-14 11:37:59 -0500

    added confirmation for delete all

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,17 @@
+import { useState } from 'react'
+
 const Footer = (props) => {
-  const clearStorage = () => {
-    localStorage.setItem('ghPlagiarismHistory', '[]')
-    props.setSearchHistory([])
+  const [clearToggle, setClearToggle] = useState(false)
+
+  const clearStorage = (e) => {
+    setClearToggle(true)
+    if (e.target.innerHTML === `I'm Sure`) {
+      localStorage.setItem('ghPlagiarismHistory', '[]')
+      props.setSearchHistory([])
+      setClearToggle(false)
+    } else if (e.target.innerHTML === `Cancel`) {
+      setClearToggle(false)
+    }
   }
 
   const toggleNotes = () => {
@@ -50,11 +60,11 @@ const Footer = (props) => {
             flex: '1 1 33.3%',
             height: '100%',
           }}
-          onClick={() => clearStorage()}
+          onClick={(e) => clearStorage(e)}
         >
           <i class='fas fa-trash-alt'></i>
         </button>
-        <button
+        {/* <button
           className='btnReset inputIcon inputIconActive'
           style={{
             flex: '1 1 33.3%',
@@ -72,13 +82,81 @@ const Footer = (props) => {
           }}
         >
           <i class='fas fa-ellipsis-h'></i>
-        </button>
+        </button> */}
       </footer>
       <div className='hidden notes-section' ref={props.notesPopup}>
         <button className='btnReset inputIcon' onClick={() => toggleNotes()}>
           <i class='fas fa-times'></i>
         </button>
       </div>
+      {clearToggle ? (
+        <div
+          style={{
+            position: 'absolute',
+            top: '15%',
+            margin: '0 10px',
+            width: '280px',
+            minHeight: '120px',
+            borderRadius: '3px',
+            backgroundColor: '#545454',
+            boxShadow: '0 0 25px #1b1b1b',
+          }}
+        >
+          <h1
+            style={{
+              padding: '0 10px',
+              fontSize: '20px',
+              textAlign: 'center',
+            }}
+          >
+            Are you sure you'd like to clear your history?
+          </h1>
+          <div
+            style={{
+              margin: 'auto',
+              width: '65%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <button
+              onClick={(e) => clearStorage(e)}
+              className='btn-reset inputIcon'
+              style={{
+                margin: '0 5px',
+                fontSize: '12px',
+                fontWeight: 'bold',
+                fontFamily: 'Noto Sans',
+                backgroundColor: '#373737',
+                border: 0,
+                color: '#e8e8e8',
+                padding: '5px 0',
+                borderRadius: '3px',
+              }}
+            >
+              I'm Sure
+            </button>
+            <button
+              onClick={(e) => clearStorage(e)}
+              className='btn-reset inputIcon'
+              style={{
+                margin: '0 5px',
+                fontSize: '12px',
+                fontWeight: 'bold',
+                fontFamily: 'Noto Sans',
+                backgroundColor: '#373737',
+                border: 0,
+                color: '#e8e8e8',
+                padding: '5px 0',
+                borderRadius: '3px',
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      ) : null}
     </>
   )
 }
