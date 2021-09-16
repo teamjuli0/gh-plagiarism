24dad30dc6cc2fd2631becf95d9afecc1f0bc60c
Julio Valdez <valdezjulio95@gmail.com>
2021-09-15 21:32:01 -0500

    updated search  input

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import helpers from '../../utils'
 import './style.css'
 
@@ -6,6 +6,7 @@ const { checkStr, handleKeyUp } = helpers
 
 const Header = (props) => {
   const inputEl = useRef()
+  const [hasText, setHasText] = useState(false)
 
   const updateHistory = (newStr) =>
     props.setSearchHistory([
@@ -17,32 +18,40 @@ const Header = (props) => {
     // whenever stop icon is pushed, reset ando focus on the input
     inputEl.current.value = ''
     inputEl.current.focus()
+    setHasText(false)
   }
 
-  const submitOnEnter = (e) =>
+  const submitOnEnter = (e) => {
+    inputEl.current.value !== '' ? setHasText(true) : setHasText(false)
     handleKeyUp(e, () => checkStr(inputEl, (str) => updateHistory(str)))
+  }
+
+  const handleInputFocus = (e) => {
+    inputEl.current.value !== '' ? setHasText(true) : setHasText(false)
+  }
 
   return (
     <header>
-      <section className='inputIconDiv'>
-        <button
-          className='btnReset inputIcon inputIconActive'
-          onClick={resetInput}
-        >
-          <i class='fas fa-times'></i>
-        </button>
-      </section>
       <section className='inputDiv'>
         <input
           ref={inputEl}
           autoFocus
           placeholder='Enter Your Query Here'
+          onClick={(e) => handleInputFocus(e)}
           onKeyUp={(e) => submitOnEnter(e)}
         />
-      </section>
-      <section className='inputIconDiv'>
+        {hasText ? (
+          <button
+            className='btnReset inputIcon inputIconActive'
+            id='clear-search'
+            onClick={resetInput}
+          >
+            <i class='fas fa-times'></i>
+          </button>
+        ) : null}
         <button
           className='btnReset inputIcon inputIconActive'
+          id='submit-search'
           onClick={() => checkStr(inputEl, (str) => updateHistory(str))}
         >
           <i className='fas fa-search'></i>
