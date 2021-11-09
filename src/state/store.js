e33e2f547cae0d9c2c6d258b3c6761699e1aca4a
Julio Valdez <valdezjulio95@gmail.com>
2021-11-08 22:07:06 -0600

    removed comment from store.js

--- src/state/store.js
+++ src/state/store.js
@@ -3,8 +3,6 @@ import { initialState } from '../utils'
 import reducers from './reducer'
 import thunk from 'redux-thunk'
 
-// const initialState = helpers
-
 export const store = createStore(
   reducers,
   initialState(),
